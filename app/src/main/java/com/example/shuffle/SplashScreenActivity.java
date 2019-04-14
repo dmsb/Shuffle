f0cc615c5ca250c97a70580759bd2eb0ac51213a
Jorge Roberto <jorgerobertof8@gmail.com>
2019-04-13 21:11:33 -0300

    Atualizando icones e animação primeira tela

--- app/src/main/java/com/example/shuffle/SplashScreenActivity.java
+++ app/src/main/java/com/example/shuffle/SplashScreenActivity.java
@@ -5,13 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageButton;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private ImageButton playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Rotation button
+        this.playButton =  findViewById(R.id.play_button);
+        float btn = this.playButton.getRotation() + 360F;
+        this.playButton.animate().rotation(btn).setInterpolator(new AccelerateDecelerateInterpolator());
+
         Handler handle = new Handler();
         handle.postDelayed(new Runnable() {
             @Override
@@ -21,6 +31,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         }, 2000);
 
     }
+
 
     private void getMainPage() {
         Intent it = new Intent(SplashScreenActivity.this,
